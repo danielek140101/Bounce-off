aa8647efb16bc792b4efacd207689e562944ebc5
Daniel <daniel.ek140101@gmail.com>
2019-04-30 13:50:41 +0200

    Small test changes

--- Bounce-off/Assets/Scripts/EnemyAI.cs
+++ Bounce-off/Assets/Scripts/EnemyAI.cs
@@ -8,24 +8,34 @@ public class EnemyAI : MonoBehaviour
     public float speed;
     bool facingRight = true;
     public float distance = 2f;
-
+    public float rotatioSpeed = 2f;
+    public Quaternion originalRotation;
     public Transform groundCheck;
     void Start()
     {
-
+        originalRotation = transform.rotation;
     }
     // Update is called once per frame
     void Update()
     {
-
-        RaycastHit2D myRay = Physics2D.Raycast(transform.position, Vector2.down);
+        //RaycastHit2D myRay = Physics2D.Raycast(transform.position, Vector2.down);
         //Debug.Log($"{myRay.collider.name}");
 
+        //EnemyMovment
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
 
-        transform.Translate(Vector2.right * speed  * Time.deltaTime);
-        RaycastHit2D ground = Physics2D.Raycast(groundCheck.position, Vector2.down, distance);
-    
+        //Check ground
+        RaycastHit2D ground = Physics2D.Raycast(groundCheck.position, Vector2.down);
 
+        //Draw a line to see RayCast
+        Debug.DrawRay(groundCheck.position, Vector3.down, Color.magenta);
+
+        // Change direction
+        Flip(ground);
+    }
+
+    private void Flip(RaycastHit2D ground)
+    {
         if (ground.collider == false)
         {
             if (facingRight)
@@ -34,7 +44,7 @@ public class EnemyAI : MonoBehaviour
                 facingRight = false;
                 Debug.Log($"Vänder vänster");
             }
-            else if( !facingRight)
+            else if (!facingRight)
             {
                 transform.eulerAngles = new Vector3(0, 0, 0);
                 facingRight = true;
---
+++ Bounce-off/Assets/Scripts/EnemyLOS.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLOS : MonoBehaviour
+{
+    public float playerRange;
+    public PlayerController player;
+    public Transform lineOfSight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawLine(new Vector3(transform.position.x - playerRange, transform.position.y, transform.position.z),
+            new Vector3(transform.position.x + playerRange, transform.position.y, transform.position.z));
+    }
+}
