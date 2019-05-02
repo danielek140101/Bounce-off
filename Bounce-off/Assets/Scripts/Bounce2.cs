f2b98a4e59b854bc621f9a4670020fe21812c354
Sofia <sofia.osterlund@bhanhof.se>
2019-05-02 14:33:54 +0200

    Bounce 2 and new controller

---
+++ Bounce-off/Assets/Scripts/Bounce2.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce2 : MonoBehaviour
+{
+    public float BounceForce = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.GetComponent<Rigidbody2D>() != null)
+        {
+            {
+                var rb = coll.gameObject.GetComponent<Rigidbody2D>();
+                rb.AddRelativeForce(new Vector2(-BounceForce, 10));
+                //rb.AddForce(transform.forward * BounceForce);
+                //rb.velocity += Vector2.left * 2;
+                //rb.AddRelativeForce(new Vector2(BounceForce,10));
+
+                Debug.Log($"{coll.gameObject.name} bouncec with force of {BounceForce}");
+            }
+        }
+    }
+
+}
---
+++ Bounce-off/Assets/Scripts/Player scripts/SofiaPlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SofiaPlayerController : MonoBehaviour
+{
+    public float speed = 10f;
+    public bool facingRight = true;
+    private Vector2 playerVelocity;
+    Rigidbody2D rb;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // rb = GetComponent<Rigidbody2D>();
+        rb = transform.GetComponent<Rigidbody2D>();
+        playerVelocity = Vector2.zero;
+    }
+
+    private void FixedUpdate()
+    {
+        float move = Input.GetAxis("Horizontal");
+        if(rb.velocity.x < speed)
+        {
+            rb.velocity += new Vector2(speed * move, 0);
+
+        }
+
+        if (move < 0 && facingRight)
+            Flip();
+
+        if (move > 0 && !facingRight)
+            Flip();
+
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(Vector3.up * 180);
+    }
+}
