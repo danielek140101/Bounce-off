8df08259b7bc2fc686a810ff9fe9e6420d6d662a
Daniel <daniel.ek140101@gmail.com>
2019-05-02 17:00:13 +0200

    DailyUpdate

--- Bounce-off/Assets/Scripts/TestPlayerInput.cs
+++ Bounce-off/Assets/Scripts/TestPlayerInput.cs
@@ -4,10 +4,7 @@ using UnityEngine;
 
 public class TestPlayerInput : MonoBehaviour
 {
-    public float speed = 0f;
-    public float acceleration = 0f;
-    public float deacceleration = 0f;
-    public float maxSpeed = 0f;
+    float speed = 30.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,18 +14,17 @@ public class TestPlayerInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.Translate(Vector2.right);
-        }
+        //if (Input.GetKey(KeyCode.RightArrow))
+        //{
+        //    transform.Translate(Vector2.right);
+        //}
 
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.Translate(Vector2.left);
-        }
+        //if (Input.GetKey(KeyCode.LeftArrow))
+        //{
+        //    transform.Translate(Vector2.left);
+        //}
 
-        //var x = Input.GetAxis("Horizontal") * Time.deltaTime * 8.0f;
-        //var y = Input.GetAxis("Vertical") * Time.deltaTime * 8.0f;
-        //transform.Translate(x, y, 0);
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        transform.Translate(x, 0, 0);
     }
 }
