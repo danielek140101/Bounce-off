f7beb8768f33cad7c12ad8e4a2f671fa1a7f20d1
Sofia <sofia.osterlund@bhanhof.se>
2019-05-03 16:38:29 +0200

    Testing AiShoot

--- Bounce-off/Assets/Scripts/AiShoot.cs
+++ Bounce-off/Assets/Scripts/AiShoot.cs
@@ -12,28 +12,39 @@ public class AiShoot : MonoBehaviour
     public GameObject bulletPrefab;
     public float fireRate = 0.5f;
     private float shotCooldown = 0.0f;
+    private Collider2D target;
+    private bool inSight = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        //Debug.Log($"My position {transform.position}");
-    }
+    //Debug.Log($"My position {transform.position}");
+}
 
     // Update is called once per frame
     void Update()
     {
-        RaycastHit2D sight = Physics2D.Raycast(transform.position, Vector2.right, 100, MaskToHit);
+        RaycastHit2D sightRight = Physics2D.Raycast(transform.position, Vector2.right, 100, MaskToHit);
+        RaycastHit2D sightLeft = Physics2D.Raycast(transform.position, Vector2.left, 100, MaskToHit);
+     
 
-        if (sight.collider != null && Time.time > shotCooldown)
+
+
+        if (sightRight.collider && Time.time > shotCooldown)
         {
+       
             shotCooldown = Time.time + fireRate;
             Instantiate(bulletPrefab, transform.position, transform.rotation);
 
+            if (sightRight.collider)
+                Debug.Log($"I shoot RIGHT {sightRight.collider.name}");
+            if (sightLeft.collider)
+                Debug.Log($"I shoot LEFT {sightLeft.collider.name}");
 
-            //Debug.Log($"time {Time.time}");
-            Debug.Log($"I see {sight.collider.name} at {sight.distance}");
-           // Debug.DrawRay(transform.position, Vector2.right, Color.green);
 
         }
+
     }
+
+
 }
